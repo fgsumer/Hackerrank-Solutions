bd1834850f37c79e44783966835827179ffe2533
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-02-22 15:23:23 +0100

    solutions of day-2

---
+++ 10 Days of Javascript/Day-2/loops.js
@@ -0,0 +1,19 @@
+function vowelsAndConsonants(s) {
+  const vowel = ['a', 'e', 'i', 'o', 'u'];
+  let vowelArray = [];
+  let consonantsArray = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (vowel.indexOf(s[i]) > -1) {
+      vowelArray.push(s[i]);
+    } else {
+      consonantsArray.push(s[i]);
+    }
+  }
+  for (let i = 0; i < vowelArray.length; i++) {
+    console.log(vowelArray[i]);
+  }
+  for (let i = 0; i < consonantsArray.length; i++) {
+    console.log(consonantsArray[i]);
+  }
+}
